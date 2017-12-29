6df5da00c90f69b2554adf49af98a2bea8a8755e
dayvesoliveira <dayvesoliveira@gmail.com>
2017-12-28 23:37:31 -0200

    Mensagem de commit

---
+++ treinamento.js
@@ -0,0 +1,15 @@
+export default class Treimanento {
+
+    /**
+     * Por convenção, o git utiliza a branch master como padrão, para o código estável do projeto. 
+     * Então, tudo o que estiver nessa branch, em qualquer repositório, ~~teóricamente~~ é código 
+     * estável. Logo, você nunca irá mexer diretamente nela, mas em outras branchs, para então 
+     * fazer merge com a master.
+     */
+
+    constructor(){
+
+    }
+
+
+}
